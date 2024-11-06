16ffacb544140d4891fd594cb373aad3b162c513
Raul Viana <raaaulviana33@gmail.com>
2024-11-06 20:07:26 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const valor = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const palos = ["♠", "♣", "♥", "♦"];
+
+  const indexNumber = Math.floor(Math.random() * valor.length);
+  const indexPalos = Math.floor(Math.random() * palos.length);
+
+  const valorRandom = valor[indexNumber];
+  const palosRandom = palos[indexPalos];
+
+  document.getElementById("number").innerText = valorRandom;
+  document.getElementById("top-card").innerText = palosRandom;
+  document.getElementById("bottom-card").innerText = palosRandom;
+
+  const topCard = document.getElementById("top-card");
+  const bottomCard = document.getElementById("bottom-card");
+
+  if (palosRandom === "♥" || palosRandom === "♦") {
+    topCard.classList.add("red");
+    bottomCard.classList.add("red");
+  } else {
+    topCard.classList.add("black");
+    bottomCard.classList.add("black");
+  }
 };
